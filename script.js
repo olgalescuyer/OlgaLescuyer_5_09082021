99400851dab0c8892501ff80e1edaa2587d5a3ad
olga <olga.lescuyer@gmail.com>
2021-08-20 09:26:57 +0200

    correct : js comment

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Create a request variable and assign a new XMLHttpRequest object to it. ICI- Déclarer une variable nommée request = création un nouveau objet.
+// Create a request variable and assign a new XMLHttpRequest object to it. ICI- Déclarer une variable nommée request = création un nouvel objet.
 var request = new XMLHttpRequest()
 
-// Initialisation. Configuration de la requête. Async car 'true'. Open a new connection, using the GET request on the URL endpoint
+// Initialisation. Configuration de la requête. open - méthode qui effectue cette requête. Async car 'true'. Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'http://localhost:3000/api/teddies', true)
 
 request.onload = function() {
     // Ecouter un événement. onload- pour des erreurs. Begin accessing JSON data here
 
-    // Begin accessing JSON data here
+    // Begin accessing JSON data here. response - body de la repponse du serveur.
     var data = JSON.parse(this.response)
 
     if (request.status >= 200 && request.status < 400) {
