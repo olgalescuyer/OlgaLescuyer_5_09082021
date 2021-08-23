f7bab3515feb3266ebe329409235aa3290162ecc
olga <olga.lescuyer@gmail.com>
2021-08-23 16:22:11 +0200

    modif : page d'accueil

--- front-end/script.js
+++ front-end/script.js
@@ -1,3 +1,55 @@
+const app = document.getElementById('root');
+
+const header = document.createElement('header');
+
+const logo = document.createElement('img');
+logo.src = './img/logo.png';
+logo.setAttribute('class', 'header__logo');
+
+const logoPanier = document.createElement('img');
+logoPanier.src = './img/panier.png';
+logoPanier.setAttribute('class', 'header__logo-panier');
+
+const headerContainer = document.createElement('div');
+headerContainer.setAttribute('class', 'container-fluid');
+headerContainer.classList.add('header__box');
+
+header.appendChild(headerContainer);
+
+headerContainer.appendChild(logo);
+headerContainer.appendChild(logoPanier);
+
+const sectionContent = document.createElement('section');
+sectionContent.setAttribute('class', 'content');
+
+const containerTeddy = document.createElement('div');
+
+containerTeddy.setAttribute('class', 'container');
+containerTeddy.classList.add('content__box');
+
+sectionContent.appendChild(containerTeddy);
+
+const containerCam = document.createElement('div');
+
+containerCam.setAttribute('class', 'container');
+containerCam.classList.add('content__box');
+
+sectionContent.appendChild(containerCam);
+
+const containerFur = document.createElement('div');
+
+containerFur.setAttribute('class', 'container');
+containerFur.classList.add('content__box');
+
+sectionContent.appendChild(containerFur);
+
+app.appendChild(header);
+app.appendChild(sectionContent);
+
+
+
+
+
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()
 
@@ -12,12 +64,105 @@ request.onload = function() {
 
     if (request.status >= 200 && request.status < 400) {
         data.forEach((teddy) => {
-            console.log(teddy.name)
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            const h1 = document.createElement('h1')
+            h1.textContent = teddy.name
+
+            const p = document.createElement('p')
+            teddy.description = teddy.description.substring(0, 300)
+            p.textContent = `${teddy.description}...`
+
+            containerTeddy.appendChild(card)
+            card.appendChild(h1)
+            card.appendChild(p)
         })
     } else {
-        console.log('error')
+        const errorMessage = document.createElement('marquee')
+        errorMessage.textContent = `Gah, it's not working!`
+        app.appendChild(errorMessage)
     }
 }
 
+
 // Envoyer la requête. GET n'a pas de body ->(0) . Send request
 request.send()
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var requestCam = new XMLHttpRequest()
+
+// Initialisation. Configuration de la requête. Open a new connection, using the GET request on the URL endpoint
+requestCam.open('GET', 'http://localhost:3000/api/cameras', true)
+
+requestCam.onload = function() {
+    // Ecouter un événement. onload- pour des erreurs. Begin accessing JSON data here
+
+    // Begin accessing JSON data here
+    var dataCam = JSON.parse(this.response)
+
+    if (requestCam.status >= 200 && requestCam.status < 400) {
+        dataCam.forEach((camera) => {
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            const h1 = document.createElement('h1')
+            h1.textContent = camera.name
+
+            const p = document.createElement('p')
+            camera.description = camera.description.substring(0, 300)
+            p.textContent = `${camera.description}...`
+
+            containerCam.appendChild(card)
+            card.appendChild(h1)
+            card.appendChild(p)
+        })
+    } else {
+        const errorMessage = document.createElement('marquee')
+        errorMessage.textContent = `Gah, it's not working!`
+        app.appendChild(errorMessage)
+    }
+}
+
+
+// Envoyer la requête. GET n'a pas de body ->(0) . Send request
+requestCam.send()
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var requestFur = new XMLHttpRequest()
+
+// Initialisation. Configuration de la requête. Open a new connection, using the GET request on the URL endpoint
+requestFur.open('GET', 'http://localhost:3000/api/furniture', true)
+
+requestFur.onload = function() {
+    // Ecouter un événement. onload- pour des erreurs. Begin accessing JSON data here
+
+    // Begin accessing JSON data here
+    var dataFur = JSON.parse(this.response)
+
+    if (requestFur.status >= 200 && requestFur.status < 400) {
+        dataFur.forEach((meubles) => {
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            const h1 = document.createElement('h1')
+            h1.textContent = meubles.name
+
+            const p = document.createElement('p')
+            meubles.description = meubles.description.substring(0, 300)
+            p.textContent = `${meubles.description}...`
+
+            containerFur.appendChild(card)
+            card.appendChild(h1)
+            card.appendChild(p)
+        })
+    } else {
+        const errorMessage = document.createElement('marquee')
+        errorMessage.textContent = `Gah, it's not working!`
+        app.appendChild(errorMessage)
+    }
+}
+
+
+// Envoyer la requête. GET n'a pas de body ->(0) . Send request
+requestFur.send()
