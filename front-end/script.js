f519fe0d5ff20b27c0669c49a9fe21b2243b2644
olga <olga.lescuyer@gmail.com>
2021-08-27 16:20:29 +0200

    supprim : XMLHttpRequest

--- front-end/script.js
+++ front-end/script.js
@@ -55,53 +55,6 @@ app.appendChild(header);
 app.appendChild(sectionContent);
 
 
-
-
-
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var request = new XMLHttpRequest()
-
-// Initialisation. Configuration de la requête. Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'http://localhost:3000/api/teddies', true)
-
-request.onload = function() {
-    // Ecouter un événement. onload- pour des erreurs. Begin accessing JSON data here
-
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
-
-    if (request.status >= 200 && request.status < 400) {
-        data.forEach((teddy) => {
-            const card = document.createElement('div')
-            card.setAttribute('class', 'card')
-
-            const img = document.createElement('img')
-            img.src = teddy.imageUrl
-            img.classList.add('content__images')
-
-            const h6 = document.createElement('h6')
-            h6.textContent = teddy.name
-
-            const p = document.createElement('p')
-            teddy.price = teddy.price / 100
-            p.textContent = `Prix: ${teddy.price}€`
-
-            containerTeddy.appendChild(card)
-            card.appendChild(img)
-            card.appendChild(h6)
-            card.appendChild(p)
-        })
-    } else {
-        const errorMessage = document.createElement('marquee')
-        errorMessage.textContent = `Gah, it's not working!`
-        app.appendChild(errorMessage)
-    }
-}
-
-
-// Envoyer la requête. GET n'a pas de body ->(0) . Send request
-request.send()
-
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var requestCam = new XMLHttpRequest()
 
@@ -144,46 +97,3 @@ requestCam.onload = function() {
 
 // Envoyer la requête. GET n'a pas de body ->(0) . Send request
 requestCam.send()
-
-// Create a request variable and assign a new XMLHttpRequest object to it.
-var requestFur = new XMLHttpRequest()
-
-// Initialisation. Configuration de la requête. Open a new connection, using the GET request on the URL endpoint
-requestFur.open('GET', 'http://localhost:3000/api/furniture', true)
-
-requestFur.onload = function() {
-    // Ecouter un événement. onload- pour des erreurs. Begin accessing JSON data here
-
-    // Begin accessing JSON data here
-    var dataFur = JSON.parse(this.response)
-
-    if (requestFur.status >= 200 && requestFur.status < 400) {
-        dataFur.forEach((meubles) => {
-            const card = document.createElement('div')
-            card.setAttribute('class', 'card')
-
-            const img = document.createElement('img')
-            img.src = meubles.imageUrl
-
-            const h6 = document.createElement('h6')
-            h6.textContent = meubles.name
-
-            const p = document.createElement('p')
-            meubles.price = meubles.price / 100
-            p.textContent = `Prix: ${meubles.price}€`
-
-            containerFur.appendChild(card)
-            card.appendChild(img)
-            card.appendChild(h6)
-            card.appendChild(p)
-        })
-    } else {
-        const errorMessage = document.createElement('marquee')
-        errorMessage.textContent = `Gah, it's not working!`
-        app.appendChild(errorMessage)
-    }
-}
-
-
-// Envoyer la requête. GET n'a pas de body ->(0) . Send request
-requestFur.send()
