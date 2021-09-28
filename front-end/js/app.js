d699e6324de872c63f960034acf7ef93650a1cfc
olga <olga.lescuyer@gmail.com>
2021-09-28 14:58:07 +0200

    ajout : des commentaires app js

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,18 +1,25 @@
 // Afficher le nombre de produit dans le header 'panier' sur toutes les pages du site (badge) :
 
 function counterBadge() {
-
+    // je demande le tableau au Storage :
     let getProduitInLocalStorage = localStorage.getItem('product');
+    // console.log(getProduitInLocalStorage);
 
+    // je fais en string :
     let allProducts = JSON.parse(getProduitInLocalStorage);
+    // console.log(allProducts);
 
+    // je demande la longueur de tableau (combien des items il contient) :
     const countAllProducts = allProducts.length;
 
+    // je fais une condition qui impose le commencement de l'execution :
     if (countAllProducts > 0) {
 
+        // j'introduit la longueur dans le html direct :
         const badgeText = document.querySelector('.header__count');
         badgeText.innerText = `( ${countAllProducts} )`;
 
+        // je modifie le comportement des éléments d'animation dans le html :
         let badgeIcon = document.querySelector('.header__icon_bag');
         badgeIcon.classList.add('invisible');
 
